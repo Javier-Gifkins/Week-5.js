c0d730831825dc49e98284859172997c7167f47a
Javier-Gifkins <javier-gifkins@live.nmit.ac.nz>
2025-03-17 10:39:17 +1300

    Learning JS strings

--- Week-5.js
+++ Week-5.js
@@ -19,3 +19,41 @@ console.log(PI)
 // console.log(Math.round(PI))
 
 console.log(Math.ceil(PI))
+
+
+console.log (Math.min(-5,6,7,3,9)) //prints -5
+console.log (Math.max(-5,6,7,3,9)) //prints 9
+
+const randNum = Math.random() //create a random number
+console.log(randNum)
+
+const num = Math.floor(Math.random() * 11) //create a random number between and 105
+console.log(num)
+
+
+console.log(Math.abs(-10)) //absolute value
+
+console.log(Math.sqrt(100)) //print square root
+
+console.log(Math.pow(3,2)) //print square root (3 to the power of 2)
+
+
+//!SECTION - Strings
+
+/*
+strings re texts which are under single or double quottions
+to declre a string we need vr nme ssignment opertor nd vlue
+*/
+
+let spce = ' '
+firstNme = "Jvier"
+lstNme = "Gifkins"
+let course = "SDV503"
+let job = "Tutor"
+
+// string conctention
+let myFullNme = firstNme + space + lstNme
+console.log(myFullNme)
+
+let newPerson = myFullNme + ". I m " + job + "teaching" + course
+console.log(newPerson)
