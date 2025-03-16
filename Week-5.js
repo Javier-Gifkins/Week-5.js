722c4ea5c6699913d13203ebc18df89e5e81f24a
Javier-Gifkins <javier-gifkins@live.nmit.ac.nz>
2025-03-17 10:55:00 +1300

    Still Learning

--- Week-5.js
+++ Week-5.js
@@ -37,9 +37,7 @@ console.log(Math.sqrt(100)) //print square root
 
 console.log(Math.pow(3,2)) //print square root (3 to the power of 2)
 
-
-//!SECTION - Strings
-
+//NOTE - Strings
 /*
 strings re texts which are under single or double quottions
 to declre a string we need vr nme ssignment opertor nd vlue
@@ -57,3 +55,45 @@ console.log(myFullNme)
 
 let newPerson = myFullNme + ". I m " + job + "teaching" + course
 console.log(newPerson)
+
+//NOTE - Template strings
+console.log(`The Sum of Two & Three is Five`)
+let a = 2
+let b = 3
+console.log(`The sum of ${a} and ${b} is ${a + b}`) // literal templates. similiar to formtted strings in python
+//  only works with backticks `` and not "" or ''
+
+console.log(`I am ${myFullNme} and im a ${job} and im in ${course}.`)
+
+
+
+//NOTE - String methods
+/*
+everything in JS is on object.  string is o doto type
+*/
+
+let JS = javscript
+
+console.log(JS.toUpperCase()) // print uppercase JS
+
+console.log(JS.toLowerCase()) // print lowercase JS
+
+console.log(JS.substr(4,6)) 
+
+
+
+
+
+// the split method
+let word = "SDV503 with javascript"
+console.log(word.split()) // tke the string nd puts it into n rry, split string in specific plse
+
+console.log(word.split(" ")) // split word into seperte indexs' put uottions on eveything in 'word'
+
+let name = "Javier Thomas Gifkins"
+console.log(name.split(" ")) // split name into seperte indexs' put uotations on eveything in 'name'
+
+
+// the trim method
+let name2 = "Gifkins   Jav"
+console.log(name.trim()) //
