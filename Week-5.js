45d1092cea0f974335fcf346e9a38a0715058c23
Javier-Gifkins <javier-gifkins@live.nmit.ac.nz>
2025-03-17 11:31:56 +1300

    Still lerning js strings

--- Week-5.js
+++ Week-5.js
@@ -105,7 +105,35 @@ console.log(name.includes("With")) // returns true or false value if u have a sp
 // the replace() method
 console.log(name.replace("Javascript", "Typescript")) // takes 2 arguments, replaces first with second
 
-let anotherWord = "Typescript"
+const anotherWord = "Typescript"
 console.log(name.replace("Type", "Java")) // takes 2 arguments, replaces Type with Java
 
+const yetAnotherWord = "Typescript"
+console.log(yetAnotherWord.replace("Type", "Java"))
+
+// charAt
+console.log(anotherWord,charAt(1))// returns value of index, in this case returns y, second char of string anotherword
+
+//charcodeat method
+console.log(anotherWord,charCodeAt(1)) // 
+
+//indexof method
+console.log(anotherWord,indexOf("S")) // shows index of S in string in this cse its t index of 4 (Type[S]cript)
+
+//lastindexof method
+console.log(anotherWord,lastIndexOf("Javascript")) 
+
+//concat()method
+console.log(anotherWord.concat("JavaScript", "C#", "C++")) // concatenates these strings together eg prints JavaScriptC#C++
+
+//the startingWith()method
+console.log(word.startsWith("SDV503")) // returns true or false if starts with that string or not
+
+//the endsWith()method
+console.log(word.startsWith("JavaScript")) // returns true or false if ends with that string or not
+
+
+// //the search() method
+// let test = "Learning HTML. CSS and Javascript in WEB504 nd lerning javscript only in SDV503"
+// console.log(test.search("Javascript")) // should return 
 
