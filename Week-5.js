cdf0b9ebada3f7d5c507cd5d52d7b4b1bbcc08ed
Javier-Gifkins <javier-gifkins@live.nmit.ac.nz>
2025-03-17 09:51:32 +1300

    Learning JS

---
+++ Week-5.js
@@ -0,0 +1,21 @@
+//NOTE - Math Object
+
+/*
+In JS the math object provides lots of methods to work with numbers.
+
+*/
+
+
+const PI = Math.PI
+console.log(PI)
+
+//!SECTION - Rounding to the closest Number:
+
+//NOTE - if above .5 then round it up. if below or less, round down
+
+// console.log(Math.round(PI))
+// console.log(Math.round(9.81))
+
+// console.log(Math.round(PI))
+
+console.log(Math.ceil(PI))
