1537b168566c90afd960bd9b02a5029d7508d99b
Javier-Gifkins <javier-gifkins@live.nmit.ac.nz>
2025-03-19 10:16:11 +1300

    arithmetic ops

---
+++ opertors.js
@@ -0,0 +1,19 @@
+//the endswith mehtod
+//
+//the search method
+//ANCHOR - Arithmetic operators
+
+let numOne = 4
+let numTwo = 3
+let sum = numOne + numTwo
+let diff = numOne - numTwo
+let mult = numOne * numTwo
+let div = numOne / numTwo
+let remainder = numOne % numTwo
+
+console.log(sum, diff, mult, div, remainder)
+
+// logicl opertors
+
+const check = 4>5 && 10>5
+console.log(check)
